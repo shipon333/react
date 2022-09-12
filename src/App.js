bb88934a5477af7059ce8fb69dfbe142d886991b
MD Shipon Mondal <shipon.cmt.eng.66@gmail.com>
2022-09-12 08:24:27 +0600

    mappgin with json data show

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Card from './components/Card';
+import Data from './data.json';
 const toDoapp = "To Do App";
 function App(){
     return(
         <div>
         <h1 className='heading'>{toDoapp}</h1>
-        <Card title="Call to Father" desc="this method call of father"/>
-        <Card title="Call to Mother" desc="this method call of Mother"/>
-        <Card title="Call to Brother" desc="this method call of Brother"/>
-        <Card title="Call to Sister" desc="this method call of Sister"/>
+        {
+            Data.map((item, index) =>
+                <Card key={index} title={item.title} desc={item.desc}/>
+            )
+        }
+        
     </div>
     )
 }
