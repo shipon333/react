44d0803310f6c7f83bb839a8076d983592878917
MD Shipon Mondal <shipon.cmt.eng.66@gmail.com>
2022-09-12 08:06:47 +0600

    Props Destructuring learn done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App(){
     return(
         <div>
         <h1 className='heading'>{toDoapp}</h1>
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Call to Father" desc="this method call of father"/>
+        <Card title="Call to Mother" desc="this method call of Mother"/>
+        <Card title="Call to Brother" desc="this method call of Brother"/>
+        <Card title="Call to Sister" desc="this method call of Sister"/>
     </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,13 @@
 
-const title = "Hello World";
-const content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley";
 const date = new Date();
 const year = date.getFullYear();
 
-function Card(){
+function Card(Props){
+    const {title, desc} = Props;
     return (
         <div className='card'>
             <h4 className='card-title'>{title}</h4>
-            <p className='card-content'>{content}</p>
+            <p className='card-content'>{desc}</p>
             <span className='card-year'>{year}</span>
       </div>
     )
